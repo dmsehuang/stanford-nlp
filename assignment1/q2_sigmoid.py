439960f81db4ebd432aedeed70a99e78493bd8c0
Huijing Huang <huijinghuang.proj@gmail.com>
2018-03-01 22:10:42 -0500

    Implement sigmoid function & gradient.

--- assignment1/q2_sigmoid.py
+++ assignment1/q2_sigmoid.py
@@ -15,7 +15,7 @@ def sigmoid(x):
     """
 
     ### YOUR CODE HERE
-    raise NotImplementedError
+    s = 1/(1 + np.exp(-x))
     ### END YOUR CODE
 
     return s
@@ -35,7 +35,7 @@ def sigmoid_grad(s):
     """
 
     ### YOUR CODE HERE
-    raise NotImplementedError
+    ds = s * (1 - s)
     ### END YOUR CODE
 
     return ds
@@ -72,8 +72,51 @@ def test_sigmoid():
     """
     print "Running your tests..."
     ### YOUR CODE HERE
-    raise NotImplementedError
+
+    print "Test case 1: scalar"
+    x = -0.99
+    f = sigmoid(x)
+    g = sigmoid_grad(f)
+    print f
+    f_ans = 0.270912078
+    assert np.allclose(f, f_ans, rtol=1e-05, atol=1e-06)
+    print g
+    g_ans = 0.197518724
+    assert np.allclose(g, g_ans, rtol=1e-05, atol=1e-06)
+    print "Test case 1: passed\n\n"
+
+    print "Test case 2: 1-d array with minor value"
+    x = np.array([4.1, -5.0])
+    f = sigmoid(x)
+    g = sigmoid_grad(f)
+    print f
+    f_ans = np.array([0.98369750, 0.00669285])
+    assert np.allclose(f, f_ans, rtol=1e-05, atol=1e-06)
+    print g
+    g_ans = np.array([0.01603673, 0.00664806])
+    assert np.allclose(g, g_ans, rtol=1e-05, atol=1e-06)
+    print "Test case 2: passed\n\n"
+
+    print "Test case 3: AxB matrix, where dim A > dim B"
+    x = np.array([[10, 1, -0.1],
+                  [4.1, -5.0, 2],
+                  [-1, -1, 1]])
+    f = sigmoid(x)
+    g = sigmoid_grad(f)
+    print f
+    f_ans = np.array([[0.99995460, 0.73105858, 0.47502081],
+                      [0.98369750, 0.00669285, 0.88079708],
+                      [0.26894142, 0.26894142, 0.73105858]])
+    assert np.allclose(f, f_ans, rtol=1e-05, atol=1e-06)
+    print g
+    g_ans = np.array([[4.53958077e-05, 0.19661193, 0.24937604],
+                      [0.01603673, 0.00664806, 0.10499359],
+                      [0.19661193, 0.19661193, 0.19661193]])
+    assert np.allclose(g, g_ans, rtol=1e-05, atol=1e-06)
+    print "Test case 3: passed\n\n"
     ### END YOUR CODE
+
+    print "All test cases passed."
 
 
 if __name__ == "__main__":
